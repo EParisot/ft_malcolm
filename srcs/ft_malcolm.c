de41c068049832e70e9dfa4ebd2638df569108f7
eparisot <eparisot@student.42.fr>
2020-10-15 19:37:46 +0200

    minor

--- srcs/ft_malcolm.c
+++ srcs/ft_malcolm.c
@@ -87,16 +87,13 @@ int			ft_malcolm(t_env *env)
 	while (g_stop == false)
 	{
 		received = recv(env->sock_fd, buf, buf_size, 0);
-		if (received > 0)
+		if (received && ft_strlen(buf) > 13 && (((buf[12]) << 8) + buf[13]) == ETH_P_ARP)
 		{
-			if (ft_strlen(buf) > 13 && (((buf[12]) << 8) + buf[13]) == ETH_P_ARP)
-			{
-				arp_frame = (struct ether_arp *) (buf + 14);
-				printf("Got an arp pkt from host with ip: %u.%u.%u.%u\n", arp_frame->arp_spa[0],
-																	arp_frame->arp_spa[1],
-																	arp_frame->arp_spa[2],
-																	arp_frame->arp_spa[3]);
-			}
+			arp_frame = (struct ether_arp *) (buf + 14);
+			printf("Got an arp pkt from host with ip: %u.%u.%u.%u\n", arp_frame->arp_spa[0],
+																arp_frame->arp_spa[1],
+																arp_frame->arp_spa[2],
+																arp_frame->arp_spa[3]);
 		}
 	}
 	return (0);

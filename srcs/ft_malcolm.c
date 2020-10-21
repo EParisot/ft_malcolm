5cb9179058fc57e5a2c31ee7339c13f2081c2fd0
eparisot <eparisot@student.42.fr>
2020-10-21 19:48:37 +0200

    allerrr!!!

--- srcs/ft_malcolm.c
+++ srcs/ft_malcolm.c
@@ -122,6 +122,7 @@ int			ft_malcolm(t_env *env)
 	char				buf[buf_size];
 	struct ether_arp 	*arp_frame;
 	t_arp_packet		*pkt = NULL;
+	struct sockaddr 	*target_addr;
 
 	ft_bzero(buf, buf_size);
 	if (getlocalhost(env))
@@ -163,9 +164,19 @@ int			ft_malcolm(t_env *env)
 		arp_frame->arp_tpa[0], arp_frame->arp_tpa[1], arp_frame->arp_tpa[2], arp_frame->arp_tpa[3],
 		env->source_mac->bytes[0], env->source_mac->bytes[1], env->source_mac->bytes[2], env->source_mac->bytes[3], env->source_mac->bytes[4], env->source_mac->bytes[5]);
 		if ((pkt = build_pkt(env)) == NULL)
+		{
+			close(env->sock_fd);
 			return (-1);
-		
-		if (sendto(env->sock_fd, pkt, sizeof(pkt), 0, (struct sockaddr*)env->target_ip, sizeof(struct sockaddr)) < 0)
+		}
+		if ((target_addr = (struct sockaddr *)malloc(sizeof(struct sockaddr))) == NULL)
+		{
+			close(env->sock_fd);
+			free(pkt);
+			return (-1);
+		}
+		ft_memcpy(target_addr, (struct sockaddr*)env->target_ip, sizeof(struct sockaddr));
+		ft_strcpy(target_addr->sa_data, env->iface);
+		if (sendto(env->sock_fd, pkt, sizeof(pkt), 0, target_addr, sizeof(struct sockaddr)) < 0)
 		{
 			printf("%s\n", strerror(errno));
 			close(env->sock_fd);

5b55eb9e1e4ff9ef5c7dfb24f88cf7d9a38bb3bc
eparisot <eparisot@student.42.fr>
2020-10-22 01:47:49 +0200

    ip_proto

--- srcs/ft_malcolm.c
+++ srcs/ft_malcolm.c
@@ -136,7 +136,7 @@ int			ft_malcolm(t_env *env)
 	ft_bzero(buf, buf_size);
 	if (getlocalhost(env))
 		return (-1);
-	if (init_sock(env, AF_PACKET, SOCK_RAW, ETH_P_ARP))
+	if (init_sock(env, AF_PACKET, SOCK_RAW, IPPROTO_RAW))
 		return (-1);
 	print_init(env);
 	g_stop = false;

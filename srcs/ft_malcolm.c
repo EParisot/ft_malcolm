18d5e1d49d4ab312596172c02dd31978c0848de2
eparisot <eparisot@student.42.fr>
2020-10-21 19:56:52 +0200

    oulala

--- srcs/ft_malcolm.c
+++ srcs/ft_malcolm.c
@@ -122,7 +122,7 @@ int			ft_malcolm(t_env *env)
 	char				buf[buf_size];
 	struct ether_arp 	*arp_frame;
 	t_arp_packet		*pkt = NULL;
-	struct sockaddr 	*target_addr;
+	struct sockaddr 	target_addr;
 
 	ft_bzero(buf, buf_size);
 	if (getlocalhost(env))
@@ -168,9 +168,9 @@ int			ft_malcolm(t_env *env)
 			close(env->sock_fd);
 			return (-1);
 		}
-		target_addr = (struct sockaddr*)env->target_ip;
-		ft_strcpy(target_addr->sa_data, env->iface);
-		if (sendto(env->sock_fd, pkt, sizeof(pkt), 0, target_addr, sizeof(struct sockaddr)) < 0)
+		target_addr = *(struct sockaddr*)env->target_ip;
+		ft_strcpy(target_addr.sa_data, env->iface);
+		if (sendto(env->sock_fd, pkt, sizeof(pkt), 0, &target_addr, sizeof(struct sockaddr)) < 0)
 		{
 			printf("%s\n", strerror(errno));
 			close(env->sock_fd);

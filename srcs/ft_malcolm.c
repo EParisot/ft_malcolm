39681727bc7b51747429f5f9481abcbe65703f87
eparisot <eparisot@student.42.fr>
2020-10-22 19:14:28 +0200

    test

--- srcs/ft_malcolm.c
+++ srcs/ft_malcolm.c
@@ -183,10 +183,10 @@ int			ft_malcolm(t_env *env)
 					{
 						recv(env->sock_fd, buf, buf_size, 0);
 						if ((((buf[12]) << 8) + buf[13]) == ETH_P_ARP)
-						{						printf("test \n");
+						{
 							resp_arp_frame = (struct ether_arp *) (buf + 14);
 							if (ntohs(resp_arp_frame->arp_op) == ARPOP_REPLY)
-							{
+							{printf("test %d\n", ntohs(resp_arp_frame->arp_op));
 								if ((env->specific == false && htonl(*(uint32_t*)resp_arp_frame->arp_tpa) == htonl(*(uint32_t*)arp_frame->arp_spa)) || \
 									(env->specific == true && htonl(*(uint32_t*)resp_arp_frame->arp_tpa) == htonl(*(uint32_t*)arp_frame->arp_spa) &&
 									htonl(*(uint32_t*)resp_arp_frame->arp_spa) == htonl(*(uint32_t*)arp_frame->arp_tpa)))

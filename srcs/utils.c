7c54dbe16f2d08605059823dd1614aa19629a4be
eparisot <eparisot@student.42.fr>
2020-10-22 12:57:36 +0200

    print fix

--- srcs/utils.c
+++ srcs/utils.c
@@ -36,10 +36,15 @@ void		print_mac(unsigned char *mac)
 
 void		print_init(t_env *env)
 {
-	
-	printf("Listening ARP packets on %s from %s ", env->iface, inet_ntoa(env->source_ip->sin_addr));
-	printf("to %s\n", inet_ntoa(env->target_ip->sin_addr));
-	
+	if (env->specific == false)
+	{
+		printf("Listening ARP packets on %s from %s to broadcast\n", env->iface, inet_ntoa(env->target_ip->sin_addr));
+	}
+	else
+	{
+		printf("Listening ARP packets on %s from %s ", env->iface, inet_ntoa(env->target_ip->sin_addr));
+		printf("to %s\n", inet_ntoa(env->source_ip->sin_addr));
+	}
 	printf("Spoof MAC : ");
 	print_mac(env->source_mac->bytes);
 }

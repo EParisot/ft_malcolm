bd22b3f18554550592e471a5dc5d8649339b03fd
eparisot <eparisot@student.42.fr>
2020-10-22 18:09:11 +0200

    aller

--- srcs/ft_malcolm.c
+++ srcs/ft_malcolm.c
@@ -101,7 +101,7 @@ void		sig_handler(int num_sig)
 	}
 }
 
-t_arp_packet	*build_pkt(unsigned char *spa, unsigned char *tpa, unsigned char *tha, bool rev)
+t_arp_packet	*build_pkt(uint8_t *spa, uint8_t *tpa, uint8_t *tha, bool rev)
 {
 	t_arp_packet *pkt;
 	t_mac empty_mac;
@@ -128,16 +128,16 @@ t_arp_packet	*build_pkt(unsigned char *spa, unsigned char *tpa, unsigned char *t
 	pkt->op             = htons(ARPOP_REPLY);
 	if (rev == false)
 	{
-		pkt->source_ip = htonl(*spa);
+		ft_memcpy(pkt->source_ip, spa, sizeof(spa));
 		ft_memcpy(pkt->source_mac, tha, sizeof(tha));
-		pkt->target_ip = htonl(*tpa);
+		ft_memcpy(pkt->target_ip, tpa, sizeof(tpa));
 		ft_memcpy(pkt->target_mac, &empty_mac, sizeof(empty_mac));
 	}
 	else
 	{
-		pkt->source_ip = htonl(*tpa);
+		ft_memcpy(pkt->source_ip, tpa, sizeof(tpa));
 		ft_memcpy(pkt->source_mac, tha, sizeof(tha));
-		pkt->target_ip = htonl(*spa);
+		ft_memcpy(pkt->target_ip, spa, sizeof(spa));
 		ft_memcpy(pkt->target_mac, &empty_mac, sizeof(empty_mac));
 	}
 	ft_bzero(pkt->padding, 18);

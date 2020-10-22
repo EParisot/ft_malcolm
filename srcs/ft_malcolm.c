58b3cf1d0fb84d7a16ae7bd7c5ac18a349813c99
eparisot <eparisot@student.42.fr>
2020-10-22 13:55:38 +0200

    all done

--- srcs/ft_malcolm.c
+++ srcs/ft_malcolm.c
@@ -215,7 +215,7 @@ int			ft_malcolm(t_env *env)
 	{
 		if (init_sock(env, AF_INET, SOCK_PACKET, ETH_P_RARP))
 			return (-1);
-		printf("Sending spoofed ARP reply for ip %u.%u.%u.%u - mac %02x:%02x:%02x:%02x:%02x:%02x\n\t\t\t\tto src ip %u.%u.%u.%u - mac %02x:%02x:%02x:%02x:%02x:%02x\n",
+		printf("Sending spoofed ARP reply with ip %u.%u.%u.%u - mac %02x:%02x:%02x:%02x:%02x:%02x\n\t\t\t\tfor ip %u.%u.%u.%u - mac %02x:%02x:%02x:%02x:%02x:%02x\n",
 			arp_frame->arp_spa[0], arp_frame->arp_spa[1], arp_frame->arp_spa[2], arp_frame->arp_spa[3],
 			env->source_mac->bytes[0], env->source_mac->bytes[1], env->source_mac->bytes[2], env->source_mac->bytes[3], env->source_mac->bytes[4], env->source_mac->bytes[5],
 			arp_frame->arp_tpa[0], arp_frame->arp_tpa[1], arp_frame->arp_tpa[2], arp_frame->arp_tpa[3],
@@ -235,7 +235,7 @@ int			ft_malcolm(t_env *env)
 		}
 		if (env->bi_directional == true)
 		{
-			printf("Sending spoofed ARP reply for ip %u.%u.%u.%u - mac %02x:%02x:%02x:%02x:%02x:%02x\n\t\t\t\tto src ip %u.%u.%u.%u - mac %02x:%02x:%02x:%02x:%02x:%02x\n",
+			printf("Sending spoofed ARP reply with ip %u.%u.%u.%u - mac %02x:%02x:%02x:%02x:%02x:%02x\n\t\t\t\tfor ip %u.%u.%u.%u - mac %02x:%02x:%02x:%02x:%02x:%02x\n",
 				arp_frame->arp_tpa[0], arp_frame->arp_tpa[1], arp_frame->arp_tpa[2], arp_frame->arp_tpa[3],
 				env->source_mac->bytes[0], env->source_mac->bytes[1], env->source_mac->bytes[2], env->source_mac->bytes[3], env->source_mac->bytes[4], env->source_mac->bytes[5],
 				arp_frame->arp_spa[0], arp_frame->arp_spa[1], arp_frame->arp_spa[2], arp_frame->arp_spa[3],

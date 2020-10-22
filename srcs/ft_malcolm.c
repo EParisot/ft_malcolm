cb6a84f85ea29c59e92d7e4f1cf5b12055d76885
eparisot <eparisot@student.42.fr>
2020-10-22 18:02:22 +0200

    brr

--- srcs/ft_malcolm.c
+++ srcs/ft_malcolm.c
@@ -128,16 +128,16 @@ t_arp_packet	*build_pkt(unsigned char *spa, unsigned char *tpa, unsigned char *t
 	pkt->op             = htons(ARPOP_REPLY);
 	if (rev == false)
 	{
-		ft_memcpy(pkt->source_ip, spa, sizeof(spa));
+		pkt->source_ip = htonl(*spa);
 		ft_memcpy(pkt->source_mac, tha, sizeof(tha));
-		ft_memcpy(pkt->target_ip, tpa, sizeof(tpa));
+		pkt->target_ip = htonl(*tpa);
 		ft_memcpy(pkt->target_mac, &empty_mac, sizeof(empty_mac));
 	}
 	else
 	{
-		ft_memcpy(pkt->source_ip, tpa, sizeof(tpa));
+		pkt->source_ip = htonl(*tpa);
 		ft_memcpy(pkt->source_mac, tha, sizeof(tha));
-		ft_memcpy(pkt->target_ip, spa, sizeof(spa));
+		pkt->target_ip = htonl(*spa);
 		ft_memcpy(pkt->target_mac, &empty_mac, sizeof(empty_mac));
 	}
 	ft_bzero(pkt->padding, 18);
